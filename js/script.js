e4b8dee583c683da29e27e874a4906d40eea0afc
amraldroubie <amraldroubie@hotmail.com>
2022-03-12 16:09:13 +0200

    editing pop up

--- js/script.js
+++ js/script.js
@@ -3,19 +3,17 @@ let this_year = date.getFullYear();
 document.querySelector("#this-year").textContent = this_year;
 
 // slide carousel to
-var myCarouselEl = document.getElementById('carousel');
-var carousel = bootstrap.Carousel.getInstance(myCarouselEl);
+var myCarousel = document.getElementById('pro-carousel');
+var carousel = new bootstrap.Carousel(myCarousel)
 
-let slide  = document.createElement("div");
-slide.classList.add("carousel-item");
-slide.classList.add("active");
+
 
 let boxes  = document.querySelectorAll(".more-example-box");
 
 boxes.forEach(box => {
     box.onclick = function(){
-        slide.innerHTML = `<img class='w-100 d-block' src = ${box.querySelector("img").getAttribute("src")}>`;
-        document.querySelector(".pro-carousel .carousel-inner").prepend(slide);
+            carousel.to(this.getAttribute('data-num')) 
     }
 } )
+
 
